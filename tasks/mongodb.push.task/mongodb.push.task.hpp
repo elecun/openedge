2d57c8effe39db0fb21251b803c50cfe7647818e
elecun <elecun2@gmail.com>
2020-04-29 11:28:27 +0900

    remove driver

--- tasks/mongodb.push.task/mongodb.push.task.hpp
+++ tasks/mongodb.push.task/mongodb.push.task.hpp
@@ -6,10 +6,18 @@
  * @author  Byunghun Hwaang<bh.hwang@iae.re.kr>
  */
 
+// requirements
+// $ sudo apt-get install libmongoc-1.0.0
+// $ sudo apt-get install libbson-1.0-0
+
+// building mongo c drvier on ubuntu
+// $ sudo apt-get install cmake libssl-dev libsasl2-dev
+
 #ifndef _OPENEDGE_MONGODB_PUSH_TASK_HPP_
 #define _OPENEDGE_MONGODB_PUSH_TASK_HPP_
 
 #include <openedge/core/task.hpp>
+#include <libmongoc-1.0/mongoc.h>
 
 using namespace oe;
 
