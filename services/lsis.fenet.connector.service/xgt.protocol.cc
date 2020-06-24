c1d48be2a59dcb1f0dab62c5bfb38880739f44c7
elecun <elecun2@gmail.com>
2020-06-24 14:36:46 +0900

    fixed for reading service configuration file

--- services/lsis.fenet.connector.service/xgt.protocol.cc
+++ services/lsis.fenet.connector.service/xgt.protocol.cc
@@ -2,6 +2,7 @@
 
 #include "xgt.protocol.hpp"
 #include <3rdparty/spdlog/spdlog.h>
+#include <algorithm>
 
 namespace oe::bus::protocol {
 
@@ -29,7 +30,7 @@ namespace oe::bus::protocol {
     vector<uint8_t> XGTDedicated::gen_read_block(const string& address, int count){
         vector<uint8_t> tmpPacket((int)HEADER_SIZE+(int)BODY_SIZE+(int)address.size()); //full length
 
-        _set_default_header(tmpPacket, cpu_info_t::XGB_IEC, sof_t::CLIENT, fenet_slot_t::SLOT1, fenet_base_t::BASE0);
+        _set_default_header(tmpPacket, XGTDedicated::_cpu_info, sof_t::CLIENT, XGTDedicated::_slot_index, XGTDedicated::_base_index);
         _set_default_body(tmpPacket, static_cast<uint16_t>(command_code_t::READ_REQUEST), static_cast<uint16_t>(datatype_t::BLOCK), 0x0001, address, (uint16_t)count);
         _packet_refine(tmpPacket);
     
@@ -39,15 +40,24 @@ namespace oe::bus::protocol {
     uint8_t XGTDedicated::chksum(vector<uint8_t>& packet){ //checksum8 modulo 256 (sum of bytes % 256)
         uint8_t sum = 0x00;
         for(int i=0;i<HEADER_SIZE-1;i++)
-        //for(int i=0;i<packet.size();i++)
             sum += packet[i];
         return (sum&0xff);
     }
 
     void XGTDedicated::setParameters(string config){
         try {
-            this->proto_configs = json::parse(config);                    
-            spdlog::info("Protocol Parameters : {}", proto_configs.dump());
+            _proto_configs = json::parse(config);
+            //config cpu info
+            if(_proto_configs.find("cpu_info")!=_proto_configs.end())
+                _cpu_info = _set_cpuInfo(_proto_configs["cpu_info"].get<std::string>());
+            //config slot index
+            if(_proto_configs.find("slot_index")!=_proto_configs.end())
+                _slot_index = _set_slotIndex(_proto_configs["slot_index"].get<int>());
+            //config base index
+            if(_proto_configs.find("base_index")!=_proto_configs.end())
+                _base_index = _set_baseIndex(_proto_configs["base_index"].get<int>());
+
+            spdlog::info("Protocol Parameters : {}", _proto_configs.dump());
         }
         catch(const json::exception& e){
             spdlog::info("Protocol Error : {}",e.what());
@@ -121,6 +131,78 @@ namespace oe::bus::protocol {
             errorcode = static_cast<xgt_errorcode_t>((uint16_t)(data[XGTProtocolIndex::ERROR_CODE+1]<<8 | data[XGTProtocolIndex::ERROR_CODE]));
         }
         return errorcode;
+    }
+
+    XGTDedicated::cpu_info_t XGTDedicated::_set_cpuInfo(const string& param){
+        string cpu = param;
+        std::for_each(cpu.begin(), cpu.end(), [](char& c) {
+        c = ::tolower(c);
+        });
+
+        std::map<string, XGTDedicated::cpu_info_t> cpus {
+            {"xgk", XGTDedicated::cpu_info_t::XGK},
+            {"xgb_mk", XGTDedicated::cpu_info_t::XGB_MK},
+            {"xgi", XGTDedicated::cpu_info_t::XGI},
+            {"xgb_iec", XGTDedicated::cpu_info_t::XGB_IEC},
+            {"xgr", XGTDedicated::cpu_info_t::XGR}
+        };
+
+        XGTDedicated::cpu_info_t _info { XGTDedicated::cpu_info_t::XGI };
+        for(std::map<string, XGTDedicated::cpu_info_t>::iterator itr=cpus.begin(); itr!=cpus.end(); ++itr){
+            if(!param.compare(itr->first)){
+                _info = itr->second;
+                break;
+            }
+        }
+
+        return _info;
+    }
+
+    XGTDedicated::fenet_slot_t XGTDedicated::_set_slotIndex(const int& index){
+        switch(index){
+            case 0: return XGTDedicated::fenet_slot_t::SLOT0; break;
+            case 1: return XGTDedicated::fenet_slot_t::SLOT1; break;
+            case 2: return XGTDedicated::fenet_slot_t::SLOT2; break;
+            case 3: return XGTDedicated::fenet_slot_t::SLOT3; break;
+            case 4: return XGTDedicated::fenet_slot_t::SLOT4; break;
+            case 5: return XGTDedicated::fenet_slot_t::SLOT5; break;
+            case 6: return XGTDedicated::fenet_slot_t::SLOT6; break;
+            case 7: return XGTDedicated::fenet_slot_t::SLOT7; break;
+            case 8: return XGTDedicated::fenet_slot_t::SLOT8; break;
+            case 9: return XGTDedicated::fenet_slot_t::SLOT9; break;
+            case 10: return XGTDedicated::fenet_slot_t::SLOT10; break;
+            case 11: return XGTDedicated::fenet_slot_t::SLOT11; break;
+            case 12: return XGTDedicated::fenet_slot_t::SLOT12; break;
+            case 13: return XGTDedicated::fenet_slot_t::SLOT13; break;
+            case 14: return XGTDedicated::fenet_slot_t::SLOT14; break;
+            case 15: return XGTDedicated::fenet_slot_t::SLOT15; break;
+            default:
+                return XGTDedicated::fenet_slot_t::SLOT0;
+        }
+    }
+
+    XGTDedicated::fenet_base_t XGTDedicated::_set_baseIndex(const int& index){
+        switch(index){
+            case 0: return XGTDedicated::fenet_base_t::BASE0; break;
+            case 1: return XGTDedicated::fenet_base_t::BASE1; break;
+            case 2: return XGTDedicated::fenet_base_t::BASE2; break;
+            case 3: return XGTDedicated::fenet_base_t::BASE3; break;
+            case 4: return XGTDedicated::fenet_base_t::BASE4; break;
+            case 5: return XGTDedicated::fenet_base_t::BASE5; break;
+            case 6: return XGTDedicated::fenet_base_t::BASE6; break;
+            case 7: return XGTDedicated::fenet_base_t::BASE7; break;
+            case 8: return XGTDedicated::fenet_base_t::BASE8; break;
+            case 9: return XGTDedicated::fenet_base_t::BASE9; break;
+            case 10: return XGTDedicated::fenet_base_t::BASE10; break;
+            case 11: return XGTDedicated::fenet_base_t::BASE11; break;
+            case 12: return XGTDedicated::fenet_base_t::BASE12; break;
+            case 13: return XGTDedicated::fenet_base_t::BASE13; break;
+            case 14: return XGTDedicated::fenet_base_t::BASE14; break;
+            case 15: return XGTDedicated::fenet_base_t::BASE15; break;
+            default:
+                return XGTDedicated::fenet_base_t::BASE0;
+        }
+
     }
 
 }
