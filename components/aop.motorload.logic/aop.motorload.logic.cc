e3e8c635ef9f7c81d20d05b733a43fac7eda64a5
elecun <elecun2@gmail.com>
2022-10-04 12:54:39 +0900

    updated

---
+++ components/aop.motorload.logic/aop.motorload.logic.cc
@@ -0,0 +1,290 @@
+
+#include "aop.uvlc.logic.hpp"
+#include <openedge/log.hpp>
+
+//static component instance that has only single instance
+static aop_uvlc_logic* _instance = nullptr;
+core::task::runnable* create(){ if(!_instance) _instance = new aop_uvlc_logic(); return _instance; }
+void release(){ if(_instance){ delete _instance; _instance = nullptr; }}
+
+void aop_uvlc_logic::execute(){
+
+    if(is_rising_l_proximity(_l_proximity_value) || is_rising_r_proximity(_r_proximity_value)){
+        move_stop();
+        console::info("Limit is ON. Motor stop. L({}), R({})", _l_proximity_value, _r_proximity_value);
+    }
+}
+
+void aop_uvlc_logic::stop(){
+
+}
+
+bool aop_uvlc_logic::configure(){
+    try {
+        const json& profile = this->get_profile()->raw();
+
+        if(profile.contains(PROFILE_CONFIGURATIONS_KEY)){
+            json config = profile[PROFILE_CONFIGURATIONS_KEY];
+
+            /* MQTT configurations */
+            if(!config.contains("mqtt")){
+                console::error("MQTT configurations does not exist");
+                return false;
+            }
+
+            json mqtt_param = config["mqtt"];
+            _broker_address = mqtt_param["broker"].get<string>();
+            _broker_port = mqtt_param["port"].get<int>();
+            _pub_topic = mqtt_param["pub_topic"].get<string>();
+            _pub_qos = mqtt_param["pub_qos"].get<int>();
+            _keep_alive = mqtt_param["keep_alive"].get<int>();
+            if(mqtt_param.find("sub_topic")!=mqtt_param.end()){
+                for(json::iterator itr=mqtt_param["sub_topic"].begin(); itr!=mqtt_param["sub_topic"].end(); ++itr){
+                    _sub_topics.emplace_back(*itr);
+                }
+            }
+
+            console::info("> set MQTT Broker : {}", _broker_address);
+            console::info("> set MQTT Port : {}", _broker_port);
+            console::info("> set MQTT Pub. Topic : {}", _pub_topic);
+            console::info("> set MQTT Pub. QoS : {}", _pub_qos);
+            console::info("> set MQTT Keep-alive : {}", _keep_alive);
+
+            //connect to MQTT broker
+            if(const int conret = this->connect_async(_broker_address.c_str(), _broker_port, _keep_alive)==MOSQ_ERR_SUCCESS){
+                for(string topic:_sub_topics){
+                    this->subscribe(nullptr, topic.c_str(), 2);
+                    console::info("> Subscribe topic : {}", topic);
+                }
+                    
+                if(const int ret = this->loop_start()!=MOSQ_ERR_SUCCESS)
+                    console::warn("({}){}", ret, mosqpp::strerror(ret));
+            }
+            else
+                console::warn("({}){}", conret, mosqpp::strerror(conret));
+
+            /* logic parameters configurations */
+            if(!config.contains("iomap")){
+                console::error("UVLC Control IO Map configurations does not exist");
+                return false;
+            }
+
+            json iomap = config["iomap"];
+            console::info("IOMap : {}", iomap.dump());
+            _l_proximity_in = iomap["l_proximity_in"].get<string>();
+            _r_proximity_in = iomap["r_proximity_in"].get<string>();
+            _wipe_forward_in = iomap["wipe_forward_in"].get<string>();
+            _wipe_backward_in = iomap["wipe_backward_in"].get<string>();
+            _wipe_stop_in = iomap["wipe_stop_in"].get<string>();
+            _emergency_in = iomap["emergency_in"].get<string>();
+
+            console::info("> set IO Map Left Proimity Sensor In : {}", _l_proximity_in);
+            console::info("> set IO Map Right Proimity Sensor In : {}", _r_proximity_in);
+            console::info("> set IO Map Wipe Forward Operation In : {}", _wipe_forward_in);
+            console::info("> set IO Map Wipe Backward Operation In : {}", _wipe_backward_in);
+            console::info("> set IO Map Wipe Stop In : {}", _wipe_stop_in);
+            console::info("> set IO Map Emergency In : {}", _emergency_in);
+
+            _wipe_forward_out = iomap["wipe_forward_out"].get<string>();
+            _wipe_backward_out = iomap["wipe_backward_out"].get<string>();
+            _wipe_stop_out = iomap["wipe_stop_out"].get<string>();
+            _emergency_out = iomap["emergency_out"].get<string>();
+
+            console::info("> set IO Map Wipe Forward Operation Out : {}", _wipe_forward_out);
+            console::info("> set IO Map Wipe Backward Operation Out : {}", _wipe_backward_out);
+            console::info("> set IO Map Wipe Stop Out : {}", _wipe_stop_out);
+            console::info("> set IO Map Emergency Out : {}", _emergency_out);
+
+        }
+        else
+            return false;
+    }
+    catch(const json::exception& e){
+        console::error("Profile read/access error : {}", e.what());
+        return false;
+    }
+
+    return true;
+}
+
+void aop_uvlc_logic::cleanup(){
+
+    /* mqtt connection close */
+    this->mosqpp::mosquittopp::disconnect();
+    this->mosqpp::mosquittopp::loop_stop();
+    mosqpp::lib_cleanup();
+
+}
+
+void aop_uvlc_logic::pause(){
+    
+}
+
+void aop_uvlc_logic::resume(){
+    
+}
+
+
+void aop_uvlc_logic::on_connect(int rc){
+    if(rc==MOSQ_ERR_SUCCESS)
+        console::info("Successfully connected to MQTT Broker({})", rc);
+    else
+        console::warn("MQTT Broker connection error : {}", rc);
+
+}
+void aop_uvlc_logic::on_disconnect(int rc){
+
+}
+void aop_uvlc_logic::on_publish(int mid){
+
+}
+void aop_uvlc_logic::on_message(const struct mosquitto_message* message){
+
+    #define MAX_BUFFER_SIZE     1024
+    char* buffer = new char[MAX_BUFFER_SIZE];
+    memset(buffer, 0, sizeof(char)*MAX_BUFFER_SIZE);
+    memcpy(buffer, message->payload, sizeof(char)*message->payloadlen);
+    string topic(message->topic);
+    string strmsg = buffer;
+    delete []buffer;
+
+    try{
+        json msg = json::parse(strmsg);
+        
+        if(msg.contains("di")){
+            json di = msg["di"];
+            if(di.contains(_l_proximity_in)) _l_proximity_value = di[_l_proximity_in].get<bool>();
+            if(di.contains(_r_proximity_in)) _r_proximity_value = di[_r_proximity_in].get<bool>();
+            if(di.contains(_wipe_forward_in)) _wipe_forward_value = di[_wipe_forward_in].get<bool>();
+            if(di.contains(_wipe_backward_in)) _wipe_backward_value = di[_wipe_backward_in].get<bool>();
+            if(di.contains(_wipe_stop_in)) _wipe_stop_value = di[_wipe_stop_in].get<bool>();
+
+            if(is_rising_forward_in(_wipe_forward_value)){
+                move_ccw();
+                console::info("Move forward from HMI");
+            }
+
+            if(is_rising_backward_in(_wipe_backward_value)){
+                move_cw();
+                console::info("Move backward from HMI");
+            }
+
+            if(is_rising_stop_in(_wipe_stop_value)){
+                move_stop();
+                console::info("Move stop from HMI");
+            }
+
+        }
+
+        /* manual command process */
+        if(msg.contains("command")){
+            string cmd = msg["command"].get<string>();
+            if(!cmd.compare("stop")) { move_stop(); }
+            else if(!cmd.compare("move_cw")){ //backward
+                if(!_r_proximity_value){
+                    move_cw();
+                    console::info("Move Backward(CW)");
+                }
+                else
+                    console::warn("Motor cannot be moved backward. Limit sensor is ON.");
+            }
+            else if(!cmd.compare("move_ccw")){ //forward
+                if(!_l_proximity_value){
+                    move_ccw();
+                    console::info("Move Forward(CCW)");
+                }
+                else
+                    console::warn("Motor cannot be moved forward. Limit sensor is ON.");
+            }
+        }
+
+    }
+    catch(json::exception& e){
+        console::error("Message Error : {}", e.what());
+    }
+    console::info("mqtt data({}) : {}",message->payloadlen, strmsg);
+
+}
+void aop_uvlc_logic::on_subscribe(int mid, int qos_count, const int* granted_qos){
+
+}
+void aop_uvlc_logic::on_unsubscribe(int mid){
+
+}
+void aop_uvlc_logic::on_log(int level, const char* str){
+
+}
+void aop_uvlc_logic::on_error(){
+    
+}
+
+bool aop_uvlc_logic::is_rising_l_proximity(const bool value){
+    static bool prev_val = false;
+    bool res = false;
+    if(prev_val==false && value==true){
+        res = true;
+    }
+    prev_val = value;
+    return res;
+}
+
+bool aop_uvlc_logic::is_rising_r_proximity(const bool value){
+    static bool prev_val = false;
+    bool res = false;
+    if(prev_val==false && value==true){
+        res = true;
+    }
+    prev_val = value;
+    return res;
+}
+
+bool aop_uvlc_logic::is_rising_forward_in(const bool value){
+    static bool prev_val = false;
+    bool res = false;
+    if(prev_val==false && value==true){
+        res = true;
+    }
+    prev_val = value;
+    return res;
+}
+
+bool aop_uvlc_logic::is_rising_backward_in(const bool value){
+    static bool prev_val = false;
+    bool res = false;
+    if(prev_val==false && value==true){
+        res = true;
+    }
+    prev_val = value;
+    return res;
+}
+
+bool aop_uvlc_logic::is_rising_stop_in(const bool value){
+    static bool prev_val = false;
+    bool res = false;
+    if(prev_val==false && value==true){
+        res = true;
+    }
+    prev_val = value;
+    return res;
+}
+
+void aop_uvlc_logic::move_cw(){
+    json cmd;
+    cmd["command"] = "move_cw"; //backward
+    string msg = cmd.dump();
+    this->publish(nullptr, _pub_topic.c_str(), strlen(msg.c_str()), msg.c_str(), 2, false); //data publish
+}
+
+void aop_uvlc_logic::move_ccw(){
+    json cmd;
+    cmd["command"] = "move_ccw"; //forward
+    string msg = cmd.dump();
+    this->publish(nullptr, _pub_topic.c_str(), strlen(msg.c_str()), msg.c_str(), 2, false); //data publish
+}
+
+void aop_uvlc_logic::move_stop(){
+    json cmd;
+    cmd["command"] = "stop"; //forward
+    string msg = cmd.dump();
+    this->publish(nullptr, _pub_topic.c_str(), strlen(msg.c_str()), msg.c_str(), 2, false); //data publish
+}
---
+++ components/aop.motorload.logic/aop.motorload.logic.hpp
@@ -0,0 +1,110 @@
+/**
+ * @file aop.uvlc.logic.hpp
+ * @author Byunghun Hwang (bh.hwang@iae.re.kr)
+ * @brief UV Lamp Cleaning System Control Logic on Openedge Framework
+ * @version 0.1
+ * @date 2022-07-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _OPENEDGE_AOP_UVLC_LOGIC_HPP_
+#define _OPENEDGE_AOP_UVLC_LOGIC_HPP_
+
+#include <openedge/core.hpp>
+#include <memory>
+#include <3rdparty/mosquitto/cpp/mosquittopp.h>
+
+using namespace oe;
+using namespace std;
+
+class aop_uvlc_logic : public core::task::runnable_rt, private mosqpp::mosquittopp {
+
+    enum class PUBLISH_METHOD : int { 
+        ON_UPDATE,
+        ON_CHANGE
+    };
+
+    /* UVLC control mode */
+    enum class CONTROLMODE : int { 
+        MANUAL = 0,
+        AUTOMATIC = 1
+    };
+
+    /* UVLC limit active check by Proximity sensors */
+    enum class LIMIT_ACTIVE : int { 
+        NO_LIMIT_ACTIVE = 0,
+        FORWARD_LIMIT_ACTIVE = 1,
+        REVERSE_LIMIT_ACTIVE = 2,
+        BOTH_LIMIT_ACTIVE = 3
+    };
+
+    /* system working state */
+    enum class UVLC_WORK_STATE : int {
+        READY = 0,
+        WORK = 1
+    };
+
+    public:
+        aop_uvlc_logic():mosqpp::mosquittopp(){};
+        ~aop_uvlc_logic() = default;
+
+        /* basic interface functions for nt */
+        virtual void execute() override;
+        virtual void stop() override;
+        virtual bool configure() override;
+        virtual void cleanup() override;
+        virtual void pause() override;
+        virtual void resume() override;
+
+    private:
+        //MQTT Callback functions
+        virtual void on_connect(int rc) override;
+		virtual void on_disconnect(int rc) override;
+		virtual void on_publish(int mid) override;
+		virtual void on_message(const struct mosquitto_message* message) override;
+		virtual void on_subscribe(int mid, int qos_count, const int* granted_qos) override;
+		virtual void on_unsubscribe(int mid) override;
+		virtual void on_log(int level, const char* str) override;
+		virtual void on_error() override;
+
+    private:
+        /* check function */
+        bool is_rising_l_proximity(const bool value);
+        bool is_rising_r_proximity(const bool value);
+        bool is_rising_forward_in(const bool value);
+        bool is_rising_backward_in(const bool value);
+        bool is_rising_stop_in(const bool value);
+        void move_cw();
+        void move_ccw();
+        void move_stop();
+
+    private:
+        bool _connected = false;
+        string _broker_address { "127.0.0.1" };
+        int _broker_port {1883};
+        string _pub_topic = {"undefined"};
+        int _pub_qos = 2;
+        int _keep_alive = {60};
+        vector<string> _sub_topics;
+
+        string _l_proximity_in;   
+        string _r_proximity_in;   //motor-side
+        string _wipe_forward_in, _wipe_forward_out;
+        string _wipe_backward_in, _wipe_backward_out;
+        string _wipe_stop_in, _wipe_stop_out;
+        string _emergency_in, _emergency_out;
+        
+        bool _l_proximity_value = false;
+        bool _r_proximity_value = false;
+        bool _wipe_forward_value = false;
+        bool _wipe_backward_value = false;
+        bool _wipe_stop_value = false;
+        
+
+}; /* end class */
+
+EXPORT_TASK_API
+
+#endif
