743a597739a1a52e43aefd0e4a774f29c3b5d6ed
elecun <elecun2@gmail.com>
2022-10-20 09:52:38 +0000

    fixed for test

--- server/oesbase/apps/aoplink/views.py
+++ server/oesbase/apps/aoplink/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 Show User Interface
 '''
 def index(request):
-    return render(request, "html/index.html")
+    addresses = [{"ip":"192.168.3.130", "name":"aop"}]
+    context = {"addresses":addresses}
+    return render(request, "html/index.html", context)
